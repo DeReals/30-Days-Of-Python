43976b4e6ac1bcdf958d4e0e1644c65389a3aa52
David Ogunbanjo <144383974+DavidSchool123@users.noreply.github.com>
2026-07-01 23:10:59 -0400

    added day 4 exercise

---
+++ 04_Day_Strings/day_4_exercise.py
@@ -0,0 +1,9 @@
+# The goal of this exercise is to mainly explore strings
+
+# multiline string: this keeps the texts in line so that everything is kept the exact way
+
+lineTexts = """Hello there,
+here is the second line
+here is the third line"""
+print(lineTexts)
+
